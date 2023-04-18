9631ca2a475f35a24ce9d91034b71d681df126d9
HARIS#10# <mdharishz107@gmail.com>
2023-04-18 15:17:50 +0530

    added Home page

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,9 +1,138 @@
 import React from "react";
+import {
+  Flex,
+  Circle,
+  Box,
+  Image,
+  Badge,
+  useColorModeValue,
+  Icon,
+  chakra,
+  Tooltip,
+} from "@chakra-ui/react";
+import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
+import { FiShoppingCart } from "react-icons/fi";
 
 const ProductCard = ({ id, title, desc, img, price }) => {
+  return (
+    <>
+      <Flex
+      // key={id}
+      >
+        <Box
+          w={"80%"}
+          m={"auto"}
+          bg={useColorModeValue("white", "gray.800")}
+          maxW="sm"
+          borderWidth="1px"
+          rounded="lg"
+          shadow="lg"
+          position="relative"
+        >
+          {/* <Circle
+              size="10px"
+              position="absolute"
+              top={2}
+              right={2}
+              bg="red.200"
+            /> */}
 
-  return <div>ProductCard</div>;
+          <Image
+            w={"100%"}
+            h={"300px"}
+            src={img}
+            alt={`Picture of ${data.name}`}
+            roundedTop="lg"
+          />
+
+          <Box p="6">
+            <Box textAlign={"left"}>
+              <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="red">
+                New
+              </Badge>
+            </Box>
+            <Flex mt="1" justifyContent="space-between" alignContent="center">
+              <Box
+                fontSize="2xl"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+              >
+                {title}
+              </Box>
+              <Tooltip
+                label="Add to cart"
+                bg="white"
+                placement={"top"}
+                color={"gray.800"}
+                fontSize={"1.2em"}
+              >
+                <chakra.a href={"#"} display={"flex"}>
+                  <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
+                </chakra.a>
+              </Tooltip>
+            </Flex>
+
+            <Flex justifyContent="space-between" alignContent="center">
+              <Box
+                fontSize="2xl"
+                color={useColorModeValue("gray.800", "white")}
+              >
+                <Box
+                  fontWeight={"bold"}
+                  as="span"
+                  color={"gray.600"}
+                  fontSize="lg"
+                >
+                  $
+                </Box>
+                {price.toFixed(2)}
+              </Box>
+            </Flex>
+          </Box>
+        </Box>
+      </Flex>
+    </>
+  );
 };
 
 export default ProductCard;
 
+const data = {
+  isNew: true,
+  imageURL:
+    "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+  name: "Wayfarer Classic",
+  price: 4.5,
+  rating: 4.2,
+  numReviews: 34,
+};
+
+// function Rating({ rating, numReviews }) {
+//   return (
+//     <Box d="flex" alignItems="center">
+//       {Array(5)
+//         .fill("")
+//         .map((_, i) => {
+//           const roundedRating = Math.round(rating * 2) / 2;
+//           if (roundedRating - i >= 1) {
+//             return (
+//               <BsStarFill
+//                 key={i}
+//                 style={{ marginLeft: "1" }}
+//                 color={i < rating ? "teal.500" : "gray.300"}
+//               />
+//             );
+//           }
+//           if (roundedRating - i === 0.5) {
+//             return <BsStarHalf key={i} style={{ marginLeft: "1" }} />;
+//           }
+//           return <BsStar key={i} style={{ marginLeft: "1" }} />;
+//         })}
+//       <Box as="span" ml="2" color="gray.600" fontSize="sm">
+//         {numReviews} review{numReviews > 1 && "s"}
+//       </Box>
+//     </Box>
+//   );
+// }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,21 +7,26 @@ const HomePage = () => {
     <>
       <Box>
         <Grid
+          p={{ base: 25, sm: 50, md: 100 }}
           templateColumns={{
             base: "repeat(1, 1fr)",
             sm: "repeat(2, 1fr)",
-            md: "repeat(4, 1fr)",
+            md: "repeat(3, 1fr)",
           }}
-          gap={{ base: "8", sm: "12", md: "16" }}
+          gap={{ base: "8", sm: "8", md: "16" }}
         >
           {products.map((ele) => {
-            <ProductCard
-              id={ele.id}
-              desc={ele.description}
-              img={ele.img}
-              title={ele.id}
-              price={ele.price}
-            />;
+            return (
+              <Box key={ele.id}>
+                <ProductCard
+                  // id={ele.id}
+                  desc={ele.description}
+                  img={ele.image}
+                  title={ele.title}
+                  price={ele.price}
+                />
+              </Box>
+            );
           })}
         </Grid>
       </Box>
